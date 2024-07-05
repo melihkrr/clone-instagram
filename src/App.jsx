0c15874f127f1024370f91c8967167611242fea2
Melih KIR <144901690+melihkrr@users.noreply.github.com>
2024-07-05 09:59:03 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Homepage from "./pages/homepage";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Homepage />
+      </div>
+    </>
+  );
+}
+
+export default App;
